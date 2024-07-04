4662f190ce3b9a6f2f0841a13089979ed549fc47
Xiaowen Zhang <xiaowen585@gmail.com>
2024-07-04 18:32:00 -0400

    added some entities

--- src/entity/Letter.java
+++ src/entity/Letter.java
@@ -1,5 +1,16 @@
 package entity;
 
 public class Letter {
-  
+  private final char letter;
+  private final int points;
+
+  public Letter(
+    char letter,
+    int points
+  ) {
+    this.letter = letter;
+    this.points = points;
+  }
+
+  public getLetter
 }
