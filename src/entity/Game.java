01da3619f33e53e185a455e31c2f7402f92a2469
alexlewis9 <150391000+alexlewis9@users.noreply.github.com>
2024-07-04 19:25:48 -0400

    create game

--- src/entity/Game.java
+++ src/entity/Game.java
@@ -1,5 +1,25 @@
 package entity;
 
+import java.util.ArrayList;
+
 public class Game {
-  
+    private static int nextId = 0;
+
+    private final int id;
+    private Board board;
+    private ArrayList<Player> players;
+    private ArrayList<Play> moves;
+    private LetterBag letterBag;
+
+    public Game() {
+        this.id = nextId++;
+        this.board = new Board();
+        this.players = new ArrayList<>();
+        this.moves = new ArrayList<>();
+        this.letterBag = new LetterBag();
+    }
+
+    public int getId() {
+        return id;
+    }
 }
