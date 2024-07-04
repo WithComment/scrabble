9b4c5c240cb388e960efa6994d0b4f06b4b238a1
Xiaowen Zhang <xiaowen585@gmail.com>
2024-07-04 19:48:30 -0400

    Created Play entity

---
+++ src/entity/Play.java
@@ -0,0 +1,38 @@
+package entity;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Play {
+  private final List<int[]> positions;
+  private final List<Letter> letters;
+  private int score;
+
+  public Play() {
+    this.positions = new LinkedList<int[]>();
+    this.letters = new LinkedList<Letter>();
+    this.score = 0;
+  }
+
+  public void addLetter(
+    int[] position,
+    Letter letter,
+    int score
+  ) {
+    this.positions.add(position);
+    this.letters.add(letter);
+    this.score += score;
+  }
+
+  public List<int[]> getPositions() {
+    return positions;
+  }
+
+  public List<Letter> getLetters() {
+    return letters;
+  }
+
+  public int getScore() {
+    return score;
+  }
+}
