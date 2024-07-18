ee120ca7ae0a3970e7890d670258dc020b1d35f6
alexlewis9 <150391000+alexlewis9@users.noreply.github.com>
2024-07-18 14:55:53 -0400

    tests for use cases

---
+++ src/test/use_case/get_leaderboard/GetLeaderboardInteractorTest.java
@@ -0,0 +1,82 @@
+package test.use_case.get_leaderboard;
+
+import entity.LeaderboardEntry;
+import entity.Player;
+import use_case.get_leaderboard.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetLeaderboardInteractorTest implements GetLeaderboardOutputBoundary{
+
+    private GetLeaderboardInputData inputData;
+    private GetLeaderboardInteractor interactor;
+    private ArrayList<LeaderboardEntry> leaderboard;
+
+    public void prepareView(GetLeaderboardOutputData outputData) {
+        leaderboard = outputData.getLeaderboard();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        interactor = new GetLeaderboardInteractor(this);
+    }
+
+    @Test
+    public void leaderboardWithMultiplePlayers_SortedDescendingByScore() {
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(new Player(1));
+        players.get(0).addScore(10);
+        players.add(new Player(2));
+        players.get(1).addScore(15);
+        players.add(new Player(3));
+        players.get(2).addScore(15);
+
+        inputData = new GetLeaderboardInputData(players);
+
+        interactor.execute(inputData);
+
+        List<LeaderboardEntry> expectedLeaderboard = new ArrayList<>();
+        expectedLeaderboard.add(new LeaderboardEntry(2, 15));
+        expectedLeaderboard.add(new LeaderboardEntry(1, 10));
+        expectedLeaderboard.add(new LeaderboardEntry(3, 5));
+
+        Assert.assertEquals(expectedLeaderboard, leaderboard);
+    }
+
+    @Test
+    public void leaderboardWithSinglePlayer_ShouldReturnSingleEntry() {
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(new Player(1));
+        players.get(0).addScore(10);
+
+        inputData = new GetLeaderboardInputData(players);
+
+        interactor.execute(inputData);
+
+        List<LeaderboardEntry> expectedLeaderboard = new ArrayList<>();
+        expectedLeaderboard.add(new LeaderboardEntry(1, 10));
+
+        Assert.assertEquals(expectedLeaderboard, leaderboard);
+    }
+
+    @Test
+    public void emptyLeaderboard_ShouldReturnEmptyList() {
+        ArrayList<Player> players = new ArrayList<>();
+
+        inputData = new GetLeaderboardInputData(players);
+
+        interactor.execute(inputData);
+
+        List<LeaderboardEntry> expectedLeaderboard = new ArrayList<>();
+
+        Assert.assertEquals(expectedLeaderboard, leaderboard);
+    }
+
+}
---
+++ src/test/use_case/get_winner/GetWinnerInteractorTest.java
@@ -0,0 +1,75 @@
+package test.use_case.get_winner;
+
+import entity.LeaderboardEntry;
+import entity.Player;
+import use_case.get_leaderboard.GetLeaderboardInputData;
+import use_case.get_winner.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetWinnerInteractorTest implements GetWinnerOutputBoundary {
+
+    private GetWinnerInputData inputData;
+    private GetWinnerInteractor interactor;
+    private ArrayList<LeaderboardEntry> winners;
+
+    public void prepareView(GetWinnerOutputData outputData) {
+        winners = outputData.getWinner();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        interactor = new GetWinnerInteractor(this);
+    }
+
+    @Test
+    public void singleWinnerIdentifiedCorrectly() {
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(new Player(1));
+        players.get(0).addScore(20);
+        players.add(new Player(2));
+        players.get(1).addScore(15);
+        inputData = new GetWinnerInputData(players);
+
+        interactor.execute(inputData);
+
+        List<LeaderboardEntry> expectedWinner = new ArrayList<>();
+        expectedWinner.add(new LeaderboardEntry(1, 20));
+        Assert.assertEquals(expectedWinner, winners);
+    }
+
+    @Test
+    public void multipleWinnersIdentifiedInCaseOfTie() {
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(new Player(1));
+        players.get(0).addScore(20);
+        players.add(new Player(2));
+        players.get(1).addScore(25);
+        inputData = new GetWinnerInputData(players);
+
+        interactor.execute(inputData);
+
+        List<LeaderboardEntry> expectedWinners = new ArrayList<>();
+        expectedWinners.add(new LeaderboardEntry(1, 20));
+        expectedWinners.add(new LeaderboardEntry(2, 20));
+        Assert.assertEquals(expectedWinners, winners);
+    }
+
+    @Test
+    public void noWinnersIdentifiedWhenNoPlayersPresent() {
+        ArrayList<Player> players = new ArrayList<>();
+        inputData = new GetWinnerInputData(players);
+
+        interactor.execute(inputData);
+
+        List<LeaderboardEntry> expectedWinners = new ArrayList<>();
+        Assert.assertEquals(expectedWinners, winners);
+    }
+}
