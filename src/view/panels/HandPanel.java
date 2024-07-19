7aa33f684442b5994511f2992ba2d9d4078df95e
Oscar Heath <136947514+baleinegris@users.noreply.github.com>
2024-07-18 22:15:21 -0400

    Replaced View with buttons, added coordinate system to buttons

--- src/view/panels/HandPanel.java
+++ src/view/panels/HandPanel.java
@@ -1,28 +1,42 @@
 package view.panels;
 
 
+import view.buttons.HandButton;
+
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.concurrent.Callable;
 
 public class HandPanel extends JPanel {
-    private ArrayList<JLabel> handLabels = new ArrayList<>();
+    private ArrayList<HandButton> handButtons = new ArrayList<>();
+
     public HandPanel() {
         setLayout(new GridLayout(1, 7));
     }
-
     public void setHand(ArrayList<String> hand) {
         removeAll();
         for(int i = 0; i < hand.size(); i++) {
-            JLabel label = new JLabel(hand.get(i));
-            label.setBorder(new LineBorder(Color.black));
-            add(label);
-            handLabels.add(label);
+            HandButton button = new HandButton(i);
+            button.setText(hand.get(i));
+            button.setPreferredSize(new Dimension(100, 100));
+            button.setBorder(new LineBorder(Color.black));
+            button.addActionListener(
+                    new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+                            System.out.println(((HandButton) e.getSource()).getPositionInHand()); //TODO: add onClick function that alerts InputManager when hand elt pressed
+                        }
+                    }
+            );
+            add(button);
+            handButtons.add(button);
         }
     }
 
     public void setHandTile(String letter, int index){
-        handLabels.get(index).setText(letter);
+        handButtons.get(index).setText(letter);
     }
 }
