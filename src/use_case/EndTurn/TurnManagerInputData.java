a111bdb8da70cee9ae76ebaae6585cbcc15ab496
Robert-0-ai <137901260+Robert-0-ai@users.noreply.github.com>
2024-07-19 13:23:59 -0400

    fixed a logic in TurnManagerInteractor

--- src/use_case/EndTurn/TurnManagerInputData.java
+++ src/use_case/EndTurn/TurnManagerInputData.java
@@ -7,10 +7,13 @@ import java.util.ArrayList;
 public class TurnManagerInputData {
         final ArrayList<Player> players;
         boolean isContestSucceed;
+        boolean isContest;
 
-    public TurnManagerInputData(ArrayList<Player> players, boolean isContestSucceed) {
+    public TurnManagerInputData(ArrayList<Player> players, boolean isContestSucceed, boolean isContest) {
         this.players = players;
         this.isContestSucceed = isContestSucceed;
+        this.isContest = isContest;
+
     }
 
     public ArrayList<Player> getPlayers() {
