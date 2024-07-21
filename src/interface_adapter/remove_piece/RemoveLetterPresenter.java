1bace40fb7ff8181fbaee27cc80dc18a874d3416
Oscar Heath <136947514+baleinegris@users.noreply.github.com>
2024-07-21 16:11:21 -0400

    Connected removePiece to view

---
+++ src/interface_adapter/remove_piece/RemoveLetterPresenter.java
@@ -0,0 +1,21 @@
+package interface_adapter.remove_piece;
+
+import interface_adapter.GameViewModel;
+import interface_adapter.ViewModel;
+import use_case.remove_letter.RemoveLetterOutputBoundary;
+import use_case.remove_letter.RemoveLetterOutputData;
+
+public class RemoveLetterPresenter implements RemoveLetterOutputBoundary {
+    private GameViewModel viewModel;
+    public RemoveLetterPresenter(GameViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+    @Override
+    public void prepareFailureView(RemoveLetterOutputData removeLetterOutputData) {
+    }
+    public void prepareSuccessView(RemoveLetterOutputData removeLetterOutputData) {
+        viewModel.setBoard(removeLetterOutputData.getBoard());
+        viewModel.setHand(removeLetterOutputData.getHand());
+        viewModel.firePropertyChanged();
+    }
+}
