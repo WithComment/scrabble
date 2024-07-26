04e0b57ee8b97329debc2e271952cea282cdd594
Robert-0-ai <137901260+Robert-0-ai@users.noreply.github.com>
2024-07-26 13:31:49 -0400

    fixed end turn use case

--- src/use_case/EndTurn/GetEndTurnInputBoundary.java
+++ src/use_case/EndTurn/GetEndTurnInputBoundary.java
@@ -3,5 +3,6 @@ package use_case.EndTurn;
 public interface GetEndTurnInputBoundary {
 
 
-    void execute(GetEndTurnOutPutData data);
+    void execute(GetEndTurnInputData getEndTurnInputData);
+
 }
