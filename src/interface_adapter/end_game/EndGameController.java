6b3f298dafddfbeeb868289eea3e066d6738efed
Rick Chen <rickchen0918@gmail.com>
2024-07-25 20:35:53 -0400

    Created Controller and Presenter for end_game use case

---
+++ src/interface_adapter/end_game/EndGameController.java
@@ -0,0 +1,16 @@
+package interface_adapter.end_game;
+
+import entity.Game;
+import use_case.end_game.EndGameInputBoundary;
+import use_case.end_game.EndGameInputData;
+
+public class EndGameController {
+    private EndGameInputBoundary interactor;
+
+    public EndGameController(EndGameInputBoundary interactor) {this.interactor = interactor;}
+
+    public void execute(Game game) throws Exception {
+        EndGameInputData endGameInputData = new EndGameInputData(game);
+        interactor.execute(endGameInputData);
+    }
+}
