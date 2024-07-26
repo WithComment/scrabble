d165ba96d8cbfbb8c776531d38528e7438fd5d24
Xiaowen Zhang <xiaowen585@gmail.com>
2024-07-26 15:28:19 -0400

    Spring-boot-fied the project

---
+++ src/main/java/com/scrabble/scrabble/ScrabbleApplication.java
@@ -0,0 +1,12 @@
+package com.scrabble.scrabble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrabbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrabbleApplication.class, args);
+	}
+}
