2fea4e5e81d0631b1f65152d43db0723e3caae41
Xiaowen Zhang <xiaowen585@gmail.com>
2024-07-31 14:55:23 -0400

    Added error handler

--- src/main/java/com/example/scrabble/use_case/confirm_play/ConfirmPlayInputBoundary.java
+++ src/main/java/com/example/scrabble/use_case/confirm_play/ConfirmPlayInputBoundary.java
@@ -1,9 +1,7 @@
 package com.example.scrabble.use_case.confirm_play;
 
-import java.io.IOException;
-
 import com.example.scrabble.entity.Game;
 
 public interface ConfirmPlayInputBoundary {
-  Game execute(ConfirmPlayInputData data) throws IOException, ClassNotFoundException;
+  Game execute(ConfirmPlayInputData data);
 }
--- src/main/java/com/example/scrabble/use_case/place_letter/PlaceLetterInputBoundary.java
+++ src/main/java/com/example/scrabble/use_case/place_letter/PlaceLetterInputBoundary.java
@@ -1,9 +1,7 @@
 package com.example.scrabble.use_case.place_letter;
 
-import java.io.IOException;
-
 import com.example.scrabble.entity.Game;
 
 public interface PlaceLetterInputBoundary {
-  Game execute(PlaceLetterInputData data) throws IOException, ClassNotFoundException;
+  Game execute(PlaceLetterInputData data);
 }
