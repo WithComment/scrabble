1026072245479dfd8a66dcff64a65f4cad26d01d
baleinegris <oscar.w.heath@gmail.com>
2024-08-04 14:52:08 -0400

    connected dispaly :)

--- frontend/src/components/Tile.jsx
+++ frontend/src/components/Tile.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import styles from './Board.module.css'
 
-function Tile({ id, intitialContent, x, y, handleClick}){
+function Tile({ id, content, x, y, handleClick}){
     const SERVER_URL = ""
     const xCoord = x;
     const yCoord = y;
-    const [content, setContent] = useState(intitialContent);
     return (
         <div className={styles.tile} id={id} key={id} onClick={(e)=>handleClick(e, xCoord, yCoord)} onContextMenu={(e) => handleClick(e, xCoord, yCoord, )}>{content}</div>
     )
