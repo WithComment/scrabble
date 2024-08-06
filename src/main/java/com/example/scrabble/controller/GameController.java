8c443b8411542e5661bf85f1c99d20e51dddc134
Xiaowen Zhang <xiaowen585@gmail.com>
2024-08-06 14:18:29 -0400

    Moved Message class to its own file

--- src/main/java/com/example/scrabble/controller/GameController.java
+++ src/main/java/com/example/scrabble/controller/GameController.java
@@ -249,38 +249,3 @@ public class GameController {
        return ResponseEntity.ok(entityModel);
    }
 }
-
-/**
- * Custom class to send STOMP messages to the frontend
- */
-class Message {
-    Object data;
-    String type;
-    
-    public Message(Object data, String type) {
-        this.data = data;
-        this.type = type;
-    }
-
-    public Object getData() {
-      return data;
-    }
-    public String getType() {
-      return type;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj){ return true; }
-
-        if (obj == null || getClass() != obj.getClass()){ return false; }
-
-        Message other = (Message) obj;
-        return (data.equals(other.data)) && (type.equals(other.type));
-    }
-
-    @Override
-    public String toString() {
-        return "Message{" + "data=" + data + ", type='" + type + '\'' + '}';
-    }
-}
