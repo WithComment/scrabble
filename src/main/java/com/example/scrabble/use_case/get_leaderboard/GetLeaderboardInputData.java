291c5375c8b50c849cb19f92c93f2890806cb5eb
alexlewis9 <150391000+alexlewis9@users.noreply.github.com>
2024-08-06 20:42:00 -0400

    make get leaderboard get players from game

--- src/main/java/com/example/scrabble/use_case/get_leaderboard/GetLeaderboardInputData.java
+++ src/main/java/com/example/scrabble/use_case/get_leaderboard/GetLeaderboardInputData.java
@@ -3,19 +3,23 @@ package com.example.scrabble.use_case.get_leaderboard;
 import java.util.List;
 
 public class GetLeaderboardInputData {
-    private List<Integer> players;
+//    private List<Integer> players;
     private int gameId;
 
     public GetLeaderboardInputData() {}
 
     public GetLeaderboardInputData(int gameId, List<Integer> players) {
         this.gameId = gameId;
-        this.players = players;
+//        this.players = players;
     }
 
-    public List<Integer> getPlayers() {
-        return players;
+    public GetLeaderboardInputData(int gameId) {
+        this.gameId = gameId;
     }
+
+//    public List<Integer> getPlayers() {
+//        return players;
+//    }
 
     public int getGameId() {
         return gameId;
